fbd4af78754c5953a249486993575795f5d2aa1a
RobJo20 <146046581+RobJo20@users.noreply.github.com>
2026-04-23 12:16:01 +1000

    (update) added select tool to tool list

--- magda-web-client/src/Components/Chatbot/tools/index.ts
+++ magda-web-client/src/Components/Chatbot/tools/index.ts
@@ -1,6 +1,7 @@
 import { ChainInput, getLocationType } from "../commons";
 import defaultAgent from "./defaultAgent";
 import searchDatasets from "./searchDatasets";
+import selectDataset from "./selectDataset";
 import { createQueryDatasetTool } from "./queryDataset";
 import { WebLLMTool } from "../ChatWebLLM";
 import { createPresentPreviousQueryResultAsChartTool } from "./presentPreviousQueryResultAsChart";
@@ -8,7 +9,11 @@ import { createPresentPreviousQueryResultAsChartTool } from "./presentPreviousQu
 async function createTools(input: ChainInput): Promise<WebLLMTool[]> {
     const type = getLocationType(input.location);
 
-    const tools: (WebLLMTool | null)[] = [searchDatasets, defaultAgent];
+    const tools: (WebLLMTool | null)[] = [
+        searchDatasets,
+        defaultAgent,
+        selectDataset
+    ];
 
     const queryTool = await createQueryDatasetTool(input);
     if (queryTool) tools.push(queryTool);
